d07ee178df421839e6b73a42030fdb73fbbdea13
IamManekiDaruma <117823090+IamManekiDaruma@users.noreply.github.com>
2024-05-25 22:08:31 +0800

    Update content.js

--- content.js
+++ content.js
@@ -1,20 +1,19 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'check_website') {
-      // Extract website content
-      let pageContent = document.body.innerText;
-  
-      // Retrieve stored goals from storage
-      chrome.storage.sync.get(['retirementGoals'], function(result) {
-        const goalsKeywords = result.retirementGoals || [];
-  
-        // Check if page content matches any of the keywords
-        let matchesGoals = goalsKeywords.some(keyword => pageContent.includes(keyword));
-  
-        // Notify the user if the page matches goals
-        if (matchesGoals) {
-          alert('This website contains information relevant to your retirement goals!');
-        }
-      });
-    }
-  });
-  
+  if (request.message === 'check_website') {
+    let pageContent = document.body.innerText;
+
+    chrome.storage.sync.get(['retirementGoals'], function(result) {
+      const goalsKeywords = result.retirementGoals || [];
+
+      let matchesGoals = goalsKeywords.some(keyword => pageContent.includes(keyword));
+
+      if (matchesGoals) {
+        chrome.runtime.sendMessage({
+          type: 'show_notification',
+          message: 'This website contains information relevant to your retirement goals!'
+        });
+      }
+    });
+  }
+});
+
