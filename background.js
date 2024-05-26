b8547748b063586b7eeb66dd293283dc7d0a871a
Kjkaikai <58503698+Kjkaikai@users.noreply.github.com>
2024-05-26 09:34:56 +0800

    add shoppee

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // background.js
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url) {
-    const shoppingSites = ["amazon.com", "ebay.com", "etsy.com", "luxury.com"];
+    const shoppingSites = ["shoppee.sg", "amazon.com", "ebay.com", "etsy.com", "luxury.com"];
     const isShoppingSite = shoppingSites.some(site => tab.url.includes(site));
 
     if (isShoppingSite) {
